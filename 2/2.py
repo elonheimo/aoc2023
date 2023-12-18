95260d75ed7b7b06611ef0efc499258220158b65
Johannes Elonheimo <37146266+elonheimo@users.noreply.github.com>
2023-12-18 22:44:42 +0200

    day 2 second start

--- 2/2.py
+++ 2/2.py
@@ -8,6 +8,11 @@ sum = 0
 def handle_game(row):
   throws = row.split(";")
   throws[0] = throws[0].split(':')[1]
+  biggest = {
+    "red":1,
+    "green":1,
+    "blue":1
+  }
   for throw in throws:
     for color, limit in [
       ("red",12),
@@ -17,17 +22,17 @@ def handle_game(row):
       finds = re.findall(f"\d*(?= {color})", throw)
       if len(finds) == 0:
         continue
-      if int(finds[0]) > limit:
-        return False
-  return True
+      if biggest[color] < int(finds[0]):
+        biggest[color] = int(finds[0])
+  return biggest['red'] * biggest['green'] * biggest['blue']
       
 
 games = {}
 sum += 0
 for row in f:
   game_id = row.split(":")[0].split(" ")[1]
-  if handle_game(row):
-    print(row)
-    sum += int(game_id)
-
+  #if handle_game(row):
+  #  print(row)
+  #  sum += int(game_id)
+  sum+=handle_game(row)
 print(sum)
